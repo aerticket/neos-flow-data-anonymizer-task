21ae3d39903a4591ca4490315927a5c32ca434ef
Thomas Heilmann <theilmann@aer.de>
2019-07-08 18:23:31 +0200

    Add scheduler task

---
+++ Classes/AnonymizeTask.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Aerticket\DataAnonymizer\Task;
+
+/*
+ * This file is part of the Aerticket.DataAnonymizer.Task package.
+ *
+ * (c) Contributors to the package
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Aerticket\DataAnonymizer\Service\AnonymizationService;
+use Neos\Flow\Annotations as Flow;
+
+/**
+ * Autoatically anonymize entities once per hour
+ *
+ * @Scheduler\Schedule(expression="8 * * * *")
+ */
+class AnonymizeTask implements \Ttree\Scheduler\Task\TaskInterface
+{
+
+    /**
+     * @var AnonymizationService
+     * @Flow\Inject()
+     */
+    protected $anonymizationService;
+
+    /**
+     * @param array
+     * @return void
+     */
+    public function execute(array $arguments = [])
+    {
+        $classNames = $this->anonymizationService->getAnonymizableClassNames();
+
+        foreach ($classNames as $className) {
+            $statistics = $this->anonymizationService->anonymize($className);
+        }
+    }
+
+}
